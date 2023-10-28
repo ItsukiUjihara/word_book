82b5d707917ff68fff06077a7c35fdfd4e702cd8
氏原一輝 <ujiharaitsuki@MacBook.local>
2023-10-28 20:42:19 +0900

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,34 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 
+  def delete
+    @user = User.find_by(id: params[:id])
+    @user = nill
+  end
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
+  
+  
+  
+  
+  
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "users#index"
-  resource :users
+  root "home#index"
+  resources :users
   get 'login', to: 'sessions#new' ,as:'new_sessions'
   post 'login', to: 'sessions#create' ,as:'create_sessions'
   delete 'logout', to: 'sessions#destroy' ,as:'destroy_sessions'
