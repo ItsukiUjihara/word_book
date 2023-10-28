f6f1e539333134b0fff0455b2596452a23af5fb9
氏原一輝 <ujiharaitsuki@MacBook.local>
2023-10-28 23:42:07 +0900

    log

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+ 
   root "home#index"
   resources :users
   get 'login', to: 'sessions#new' ,as:'new_sessions'
   post 'login', to: 'sessions#create' ,as:'create_sessions'
-  delete 'logout', to: 'sessions#destroy' ,as:'destroy_sessions'
-  
+  get 'logout', to: 'sessions#destroy' ,as:'destroy_sessions'
   
 end
