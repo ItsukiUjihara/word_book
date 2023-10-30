29b72b47d04b7f8d60ed795086972b7a1c2908dc
氏原一輝 <ujiharaitsuki@MacBook.local>
2023-10-30 11:41:17 +0900

    log

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "ログインしました。"
+      redirect_to root_path
     else
       # ログイン失敗時の処理
-      flash.now[:alert] = "メールアドレスまたはパスワードが間違っています。"
       render 'new'
     end
   end
@@ -18,7 +17,7 @@ class SessionsController < ApplicationController
   def destroy
     # ログアウト処理
     session[:user_id] = nil
-    redirect_to new_sessions_path, notice: "ログアウトしました。"
+    redirect_to new_sessions_path
   end
 
 end
